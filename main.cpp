708bad8cc59bd9eaf68f8f633b102230c80d7e87
HASSANEIN Alzahra <h13003041@l-024110a018-20.local>
2018-09-18 17:56:42 +0200

    mouvement et makefile pour box2d

--- main.cpp
+++ main.cpp
@@ -3,17 +3,22 @@
 
 #include <GLUT/glut.h>
 #include <stdlib.h>
+//#include <Box2D/Box2D.h>
 
 /** Pensez a faire :
+ 
+ Tourner l'affichage pour une initialisation a plat
+ Faire une fonction init
  
  FAIT  Matrices pour la boule
  FAIT  Matrice pour le plateau
  Voir si le soucis de couleur de la boule est regle, par defaut la couleur verte
-    du dernier objet dessine
+ du dernier objet dessine
  RESOLU  Nouveau Problème : Disparition de l'axe Z (bleu) a l'affichage
  
  FAIT Deplacer les axes en bas a gauche de la fenetre
- Voir si on peut faire un axe qui fait moins "attacher" au plan
+ RESOLU Voir si on peut faire un axe qui fait moins "attacher" au plan
+    Il sera enlever pour la fin
  
  RESOLU Nouveau probleme : Translation de la boule sur le plateau ne fonctionne pas
     On dirait que la translation sur Z pose un soucis, on a l'effet de zoom mais
@@ -22,18 +27,31 @@
     au lieu de la deplacer
  
  FAIT  Enlevez les a et remplacer par SECTION
- Uniformisez la création du plateau ( et déplacer par Translation et non pas
+ Uniformisez la création du plateau (et déplacer par Translation et non pas
  recreer par coordonnées)
  
  FAIT Creer des variables pour les angles de rotations du plateau,
     cf Fontion Rotation
  
  FAIT Creer une fonction mouvement pour la boule,
- Definir la vitesse a partir de l'angle de Rotation du plateau
- Definir a partir de quel angle la boule ne bougera plus (plus de 90 la boule
+ REDEFINI Definir la vitesse a partir de l'angle de Rotation du plateau
+    Faire un deplacecment plus naturel
+ 
+ RESOLU Definir a partir de quel angle la boule ne bougera plus (plus de 90 la boule
  devrait "tomber" du plateau,
+    On ne bouge pas en Z pour le moment, la balle reste collé et se deplace en fonction meme
+    a l'envers, prendre apres 180 degre, inverser les deplacements
  
  Passer en GLFW, necessaire ?
+ Etudier BOX2D pour
+    la physique
+    et le deplacement coulissant
+    et la gestion des collisions (premiere collision mur du bord donc largeur (ou section)
+        plus ou moins le rayon esst la distance maximale, suite collision au mur, il faut detecter
+        si on est pas sur une coordoonées d'une ligne d'un mur, donc avec le tableau de ligne
+ 
+ Faire des murs avec une epaisseur,
+ Faire un tableau de ligne pour dessiner les murs internes
  
  Commenter
  
@@ -53,18 +71,18 @@ void def_axes(void){
     
     
     glBegin(GL_LINES);
-        //x
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex3f(-1.,0.0,0.0);
-        glVertex3f(1.,0.0,0.0);
-        //y
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex3f(0.0,-1,0.0);
-        glVertex3f(0.0,1.,0.0);
-        //z
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3f(0.0,0.0,-1.);
-        glVertex3f(0.0,0.0,1.);
+    //x
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(-1.,0.0,0.0);
+    glVertex3f(1.,0.0,0.0);
+    //y
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0,-1,0.0);
+    glVertex3f(0.0,1.,0.0);
+    //z
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0,0.0,-1.);
+    glVertex3f(0.0,0.0,1.);
     glEnd();
     
 }
@@ -73,10 +91,10 @@ void def_axes(void){
 void def_carre(void){
     int a = 1;
     glBegin(GL_POLYGON);
-        glVertex2f(-a/2.0,a/2.0);
-        glVertex2f(a/2.0,a/2.0);
-        glVertex2f(a/2.0,-a/2.0);
-        glVertex2f(-a/2.0 ,-a/2.0);
+    glVertex2f(-a/2.0,a/2.0);
+    glVertex2f(a/2.0,a/2.0);
+    glVertex2f(a/2.0,-a/2.0);
+    glVertex2f(-a/2.0 ,-a/2.0);
     glEnd();
     
 }
@@ -85,11 +103,11 @@ void def_carre(void){
 void def_plateau(void){
     
     glPushMatrix();
-        glColor3f(0.0, 0.5, 1.0);
-        glTranslatef(0.0, 0.0, -SECTION/4.0);
-        glRotatef(180, 1, 0, 0);
-        glScalef(SECTION*3,SECTION*2.,SECTION);
-        def_carre();
+    glColor3f(0.0, 0.5, 1.0);
+    glTranslatef(0.0, 0.0, -SECTION/4.0);
+    glRotatef(180, 1, 0, 0);
+    glScalef(SECTION*3,SECTION*2.,SECTION);
+    def_carre();
     glPopMatrix();
 }
 
@@ -100,55 +118,55 @@ void def_boite(void){
     // face bleu, le fond
     //on le met devant en translatant selon z
     glPushMatrix();
-        glColor3f(0.0, 0.1, 1.0);
-        glTranslatef(0.0, 0.0, -SECTION/4.0);
-        glRotatef(180, 1, 0, 0);
-        glScalef(LONGUEUR,SECTION,SECTION);
-        def_carre();
+    glColor3f(0.0, 0.1, 1.0);
+    glTranslatef(0.0, 0.0, -SECTION/4.0);
+    glRotatef(180, 1, 0, 0);
+    glScalef(LONGUEUR,SECTION,SECTION);
+    def_carre();
     glPopMatrix();
     
     // face rouge, de droite
     //rotation de 90 selon l'axe des y et translation sur les x et z
     glPushMatrix();
-        glColor3f(1.0, 0.0, 0.0);
-        glTranslatef(LONGUEUR/2.0, 0.0, 0.0);
-        glRotatef(90, 0, 1, 0);
-        glScalef(SECTION/2.,SECTION,SECTION);
-        //glScalef(SECTION,SECTION,SECTION/2.0);
-        //glScalef(LONGUEUR*a,SECTION,SECTION);
-        def_carre();
+    glColor3f(1.0, 0.0, 0.0);
+    glTranslatef(LONGUEUR/2.0, 0.0, 0.0);
+    glRotatef(90, 0, 1, 0);
+    glScalef(SECTION/2.,SECTION,SECTION);
+    //glScalef(SECTION,SECTION,SECTION/2.0);
+    //glScalef(LONGUEUR*a,SECTION,SECTION);
+    def_carre();
     glPopMatrix();
     
     // face jaune, de gauche
     glPushMatrix();
-        glColor3f(1.0, 1.0, 0.0);
-        glTranslatef(-LONGUEUR/2.0, 0.0,0.0);
-        glRotatef(-90, 0, 1, 0);
-        glScalef(SECTION/2.,SECTION,SECTION);
-        //glScalef(SECTION,LONGUEUR*a,SECTION);
-        //glScalef(LONGUEUR*a,SECTION,SECTION);
-        def_carre();
+    glColor3f(1.0, 1.0, 0.0);
+    glTranslatef(-LONGUEUR/2.0, 0.0,0.0);
+    glRotatef(-90, 0, 1, 0);
+    glScalef(SECTION/2.,SECTION,SECTION);
+    //glScalef(SECTION,LONGUEUR*a,SECTION);
+    //glScalef(LONGUEUR*a,SECTION,SECTION);
+    def_carre();
     glPopMatrix();
     
     // face blanche, de haut
     
     glPushMatrix();
-        glColor3f(1.0, 1.0, 1.0);
-        glTranslatef(0.0, SECTION/2.0, 0.0);
-        glRotatef(-90, 1, 0, 0);
-        glScalef(LONGUEUR,SECTION/2.,SECTION);
-        //glScalef(SECTION,LONGUEUR*a,SECTION);
-        def_carre();
+    glColor3f(1.0, 1.0, 1.0);
+    glTranslatef(0.0, SECTION/2.0, 0.0);
+    glRotatef(-90, 1, 0, 0);
+    glScalef(LONGUEUR,SECTION/2.,SECTION);
+    //glScalef(SECTION,LONGUEUR*a,SECTION);
+    def_carre();
     glPopMatrix();
     
     //face de bas, verte
     glPushMatrix();
-        glColor3f(0.0, 1.0, 0.0);
-        glTranslatef(0.0, -SECTION/2.0, 0.0);
-        glRotatef(90, 1, 0, 0);
-        glScalef(LONGUEUR,SECTION/2.,SECTION);
-        //glScalef(SECTION,LONGUEUR*a,SECTION);
-        def_carre();
+    glColor3f(0.0, 1.0, 0.0);
+    glTranslatef(0.0, -SECTION/2.0, 0.0);
+    glRotatef(90, 1, 0, 0);
+    glScalef(LONGUEUR,SECTION/2.,SECTION);
+    //glScalef(SECTION,LONGUEUR*a,SECTION);
+    def_carre();
     glPopMatrix();
     
 }
@@ -157,21 +175,21 @@ void def_boite(void){
 
 void RotationM(float M[16], int valaxe, float angle) {
     glPushMatrix();
-        glLoadIdentity();
-        glLoadMatrixf(M);
+    glLoadIdentity();
+    glLoadMatrixf(M);
     
-        switch (valaxe) {
-            case 1: //x
-                glRotatef(angle, 1.0, 0.0, 0.0);
-                break;
-            case 2: //y
-                glRotatef(angle, 0.0, 1.0, 0.0);
-                break;
-            case 3://z
-                glRotatef(angle, 0.0, 0.0, 1.0);
-                break;
-        }
-        glGetFloatv(GL_MODELVIEW_MATRIX, M);
+    switch (valaxe) {
+        case 1: //x
+            glRotatef(angle, 1.0, 0.0, 0.0);
+            break;
+        case 2: //y
+            glRotatef(angle, 0.0, 1.0, 0.0);
+            break;
+        case 3://z
+            glRotatef(angle, 0.0, 0.0, 1.0);
+            break;
+    }
+    glGetFloatv(GL_MODELVIEW_MATRIX, M);
     glPopMatrix();
 }
 
@@ -202,39 +220,43 @@ void clavier(unsigned char key, int x, int y){
         case 'q':
         case 'Q':
             RotationM(M_plateau,2,-10);
+            MouvementM(M_boule,1,-1);
             break;
         case 'd':
         case 'D':
             RotationM(M_plateau,2,10);
+            MouvementM(M_boule,1,1);
             break;
         case 'z':
         case 'Z':
             RotationM(M_plateau,1,-10);
+            MouvementM(M_boule,2,1);
             break;
         case 's':
         case 'S':
             RotationM(M_plateau,1,10);
+            MouvementM(M_boule,2,-1);
             break;
-        
-        
-     
-        //Zoom
+            
+            
+            
+            //Zoom
         case 'b':
         case 'B':
             MouvementM(M_boule,3,0.1);
             break;
-        
+            
             //Dezoom de la balle
         case 'n':
         case 'N':
-            MouvementM(M_boule,3,0.1);
+            MouvementM(M_boule,3,-0.1);
             break;
-        /*
-        case 'i':
-        case 'I':
-            //Initialisation();
-            break;
-        */
+            /*
+             case 'i':
+             case 'I':
+             //Initialisation();
+             break;
+             */
         case 'a':
         case'A' :
             exit (0);
@@ -244,30 +266,30 @@ void clavier(unsigned char key, int x, int y){
     }glutPostRedisplay();
 }
 
-void clavierBoule(int key, int x, int y){
-    switch (key){
-        case GLUT_KEY_UP:
-            MouvementM(M_boule,2,0.1);
-            break;
-        case GLUT_KEY_DOWN:
-            MouvementM(M_boule,2,-0.1);
-            break;
-        case GLUT_KEY_LEFT:
-            MouvementM(M_boule,1,-0.1);
-            break;
-        case GLUT_KEY_RIGHT:
-            MouvementM(M_boule,1,0.1);
-            break;
-    }glutPostRedisplay();
-            
-}
+//void clavierBoule(int key, int x, int y){
+//    switch (key){
+//        case GLUT_KEY_UP:
+//            MouvementM(M_boule,2,0.1);
+//            break;
+//        case GLUT_KEY_DOWN:
+//            MouvementM(M_boule,2,-0.1);
+//            break;
+//        case GLUT_KEY_LEFT:
+//            MouvementM(M_boule,2,-0.1);
+//            break;
+//        case GLUT_KEY_RIGHT:
+//            MouvementM(M_boule,1,0.1);
+//            break;
+//    }glutPostRedisplay();
+//
+//}
 
 void def_boule(void){
     
     glPushMatrix();
-        //float blanc[] = { 1.0F,1.0F,1.0F,1.0F };
-        //glMaterialfv(GL_FRONT,GL_SPECULAR,blanc);
-        glutSolidSphere(0.3,36,36);
+    //float blanc[] = { 1.0F,1.0F,1.0F,1.0F };
+    //glMaterialfv(GL_FRONT,GL_SPECULAR,blanc);
+    glutSolidSphere(0.3,36,36);
     glPopMatrix();
     
 }
@@ -278,24 +300,27 @@ void affichage(void){
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPolygonMode(GL_BACK, GL_LINE); // a enlever par la suite
     
+    
+    
     glPushMatrix();
-        glMultMatrixf(M_plateau);
-        //glTranslatef(-3.0,-3.0,0.0);
-        def_axes();
+    glMultMatrixf(M_plateau);
+    //glTranslatef(-3.0,-3.0,0.0);
+    def_axes();
     glPopMatrix();
     
     glPushMatrix();
-        glMultMatrixf(M_plateau);
-        def_plateau();
-        def_boite();
+    
+    glMultMatrixf(M_plateau);
+    def_plateau();
+    def_boite();
     glPopMatrix();
     
     glPushMatrix();
-        //
-        glMultMatrixf(M_plateau);
-        glTranslatef(-0.3-SECTION/2., -0.3+SECTION/2., 0.3-SECTION/4.0);
-        glMultMatrixf(M_boule);
-        def_boule();
+    //
+    glMultMatrixf(M_plateau);
+    glTranslatef(-0.3-SECTION/2., -0.3+SECTION/2., 0.3-SECTION/4.0);
+    glMultMatrixf(M_boule);
+    def_boule();
     glPopMatrix();
     
     
@@ -318,7 +343,7 @@ void redim(int width, int height){
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    
+
     //Initialisation des matrices a Identite
     glGetFloatv(GL_MODELVIEW_MATRIX,M_plateau);
     glGetFloatv(GL_MODELVIEW_MATRIX,M_boule);
@@ -350,13 +375,10 @@ int main (int argc, char **argv){
     
     //Action Clavier
     glutKeyboardFunc(clavier);
-    glutSpecialFunc(clavierBoule);
-
-    //glutMouseFunc(souris);
+    //glutSpecialFunc(clavierBoule);
     
     glutMainLoop();
     
     return 0;
     
 }
-
--- makefile
+++ makefile
@@ -1,23 +1,24 @@
-UNAME_S := $(shell uname -s)
+ UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
 	OPENCV_LIBS = -L/usr/local/lib -pthread -lopencv_imgcodecs -lopencv_imgproc -lopencv_core -lopencv_highgui
 	GLFW_LIBS = -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
 	INC = -I/usr/local/include/opencv2/
-	OSX_LIBS=
-	FLAGS = -lGL -lGLU -lglut
+	FLAGS = -lGL -lGLU -lglut -lBox2d
 endif
 
 ifeq ($(UNAME_S),Darwin)
-	FLAGS = -framework OpenGL -framework glut
+	OPENCV_LIBS = -L../lib/ -lIlmImf -llibtiff -lopencv_imgcodecs -lippicv -llibwebp -lopencv_imgproc -llibjasper -lopencv_core -lzlib -llibjpeg -lopencv_hal -llibpng -lopencv_highgui 
+	BOX2D_LIBS = -L../lib/ -lBox2D
+	INC = -I../include/
+	FLAGS = -framework OpenGL -framework glut -framework OpenCL -framework Cocoa 
+
 endif
 
 all: main
 
-main: main.o 
-	$(CXX) -o $@ main.o $(FLAGS) 
-
-main.o: main.cpp 
-	$(CXX) -c main.cpp $(FLAGS) 
+main: main.cpp 
+#	$(CXX) -o main main.cpp $(INC) $(FLAGS) $(OPENCV_LIBS) $(BOX2D_LIBS) 
+	$(CXX) -o main main.cpp $(FLAGS)  
 
 clean:
-	rm *.o main
+	rm main
