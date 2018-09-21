0826a373e2e087ba8312ae3150b77d033284e9f9
Raphael-Os <20703359+Raphael-Os@users.noreply.github.com>
2018-09-21 15:41:22 +0200

    Add files via upload

--- analyse_dedale.cpp
+++ analyse_dedale.cpp
@@ -22,7 +22,7 @@ int max_Trackbar = 5;
 
 cv::Mat image;
 cv::Mat model;
-Mat image_rect, image_circle;
+Mat image_rect, image_circle, image_walls;
 
 int treshold_value = 0;
 int cpt_frame = 0;
@@ -132,7 +132,7 @@ void findPoly(Mat image) {
     
     cvtColor(image, gray, CV_BGR2GRAY);
     medianBlur(gray, blur, 3);
-    Canny(blur, bw, 0, 50);
+    Canny(blur, bw, 50, 150, 3);
     
     // dilate to ensure there is no cut off lines
     dilate(bw, dil, Mat(), Point(-1,-1));
@@ -170,7 +170,55 @@ void findPoly(Mat image) {
     imshow("a", image);
     resizeWindow("a", 900, 600);
 }
-               
+
+//Si a est compris dans l'interface [b +- inter]
+bool compris(int a, int b, int inter) {
+    if( b - inter <= a && b + inter >= a)
+        return true;
+    return false;
+}
+
+void findWalls(Mat image) {
+    
+    Mat gray;
+    int inter = 40;
+    image_walls = image.clone();
+    cvtColor(image_walls, gray, CV_BGR2GRAY);
+    //cvtColor(image_walls, image_walls, CV_BGR2GRAY);
+
+    medianBlur(gray, gray, 3);
+    Canny(gray, gray, 50, 150, 3);
+    
+    // dilate to ensure there is no cut off lines
+    //dilate(gray, gray, Mat(), Point(-1,-1));
+    
+    vector<Vec4i> lines;
+    
+    HoughLinesP( gray, lines, 1, CV_PI/180, 80, image.rows/20, image.rows/15);
+    cout << lines[0][0] << endl;
+    
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        for( size_t j = 0; j < lines.size(); j++ )
+        {
+            if(compris(lines[i][0], lines[j][0], inter) &&  compris(lines[i][1], lines[j][1], inter) &&
+               compris(lines[i][2], lines[j][2], inter) &&  compris(lines[i][3], lines[j][3], inter)) {
+                lines.erase(lines.begin() + j);
+            }
+        }
+    }
+    
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        cout << lines[i] << endl;
+        line( image_walls, Point(lines[i][0], lines[i][1]),
+             Point(lines[i][2], lines[i][3]), Scalar(0,255,255), 3, 8 );
+    }
+    
+    imshow( "Walls", image_walls );
+}
+
+
 int main( int argc, char **argv ) {
     
     if ( argc < 2 ) {
@@ -178,14 +226,36 @@ int main( int argc, char **argv ) {
         return -1;
     }
     
-    image       = imread( argv[1]);
-    image_rect  = Mat(image.rows, image.cols, CV_8UC3, cv::Scalar(255,0,0));
-    image_circle  = Mat(image.rows, image.cols, CV_8UC3, cv::Scalar(255,0,0));
+    image           = imread( argv[1]);
+    image_rect      = Mat(image.rows, image.cols, CV_8UC3, cv::Scalar(255,0,0));
+    image_circle    = Mat(image.rows, image.cols, CV_8UC3, cv::Scalar(255,0,0));
+    image_walls     = Mat(image.rows, image.cols, CV_8UC3, cv::Scalar(255,0,0));
     
     if ( !image.data ) { /* Check for invalid input */
         printf("Could not open or find the image\n") ;
         return -1;
     }
+    
+    Mat mask = image.clone();
+    /*
+    
+    rectangle(mask,Point(0,0), Point(image.cols,image.rows/10), Scalar(255,255,255), CV_FILLED, 1, 0 );
+    rectangle(mask,Point(0,0), Point(image.cols/10,image.rows), Scalar(255,255,255), CV_FILLED, 1, 0 );
+    rectangle(mask,Point(image.cols,image.rows), Point(image.cols/10,image.rows-image.rows/10), Scalar(255,255,255), CV_FILLED, 1, 0 );
+    rectangle(mask,Point(image.cols,image.rows), Point(image.cols-image.cols/10,image.rows/10), Scalar(255,255,255), CV_FILLED, 1, 0 );
+    
+   
+    
+    threshold(mask, mask, 254, 255, 0 ); //Seuillage
+     */
+    /*
+    rectangle(mask, Point(image.cols-image.cols/10, image.rows-image.rows/10),
+    Point(image.cols/10,image.rows/10), Scalar(255,255,0), 5, 1, 0 );
+     */
+    
+    findWalls(image);
+    
+    
     
     // Trouver les contours externes du labyrinthe et les affichers
     
@@ -211,6 +281,15 @@ int main( int argc, char **argv ) {
     namedWindow("Circle", 0);
     imshow( "Circle", image_circle );
     resizeWindow("Circle", 900, 600);
+    
+    namedWindow("Mask", 0);
+    imshow( "Mask", mask );
+    resizeWindow("Mask", 900, 600);
+    
+    namedWindow("Walls", 0);
+    imshow( "Walls", image_walls );
+    resizeWindow("Walls", 900, 600);
+    moveWindow("Walls", 30, 30);
     
     createTrackbar( "Seuil : ", "Rectangle", &treshold_value, 255, Contour );
 
