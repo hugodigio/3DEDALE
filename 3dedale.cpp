d2b4c43e2127d85a7013bdbd891d380fae11e580
FARAUD <baptiste.faraud@gmail.com>
2018-10-09 11:08:51 +0200

    Add files via upload

--- 3dedale.cpp
+++ 3dedale.cpp
@@ -11,8 +11,6 @@ using std::vector;
 
 CvCapture* capture;
 
-int treshold_value = 40;
-
 int nombre_de_trackers = 4;
 
 Mat homography;
@@ -39,73 +37,6 @@ Mat getImage () {
     Mat image = cvarrToMat(frame);
 	return image;
 }
-
-/*
-Rect contoursConvexHull( Mat image, int tresh)
-{
-    Mat image_convex;
-    vector<vector<Point> > vec_contours;
-
-    cvtColor(image, image_convex, CV_BGR2GRAY);
-    threshold( image_convex, image_convex, tresh, 255, 0 ); //Seuillage
-    blur(image_convex, image_convex, Size(3, 3));
-    
-    Canny(image_convex, image_convex, 0, 100, 3, true);
-    
-    findContours( image_convex, vec_contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE );
- 
-    vector<Point> ConvexHullPoints;
-    vector<Point> pts;
-    
-    for ( size_t i = 0; i< vec_contours.size(); i++)
-        for ( size_t j = 0; j< vec_contours[i].size(); j++)
-            pts.push_back(vec_contours[i][j]);
-    convexHull( pts, ConvexHullPoints );
-    
-    Rect bounding_rect = boundingRect (ConvexHullPoints);
-    
-    x_init = bounding_rect.x;
-    y_init = bounding_rect.y;
-    width_init = bounding_rect.width;
-    height_init = bounding_rect.height;
-    
-    return bounding_rect;
-}
-
-void old_detection ()
-{
-	while(1){
-		Mat img = getImage();
-		flip(img, img, 1);
-		Mat image_lines = img.clone();
-    
-		Rect pts_contours = contoursConvexHull(img, treshold_value);
-    
-		rectangle(image_lines, pts_contours, (127,255,0), 5);
-		imshow( "Camera", image_lines );
-		char c = cvWaitKey(33);
-		if( c == 27 ) break;
-	}
-	
-	Point p;
-	
-	p.x = x_init;
-	p.y = y_init;
-	points_initiaux.push_back(p);
-	
-	p.x = x_init + width_init;
-	p.y = y_init;
-	points_initiaux.push_back(p);
-	
-	p.x = x_init;
-	p.y = y_init + height_init;
-	points_initiaux.push_back(p);
-	
-	p.x = x_init + width_init;
-	p.y = y_init + height_init;
-	points_initiaux.push_back(p);
-}
-*/
 
 void detection() {
 	Mat img = getImage();
@@ -159,13 +90,10 @@ void detection() {
 
 
 void tracking() {
-    // Read first frame 
     Mat frame = getImage();  
     flip(frame, frame, 1);
  
-    // Define initial bounding box 
-    // x y width height
-    
+    // x y width height   
     Rect bboxes[nombre_de_trackers];
     Rect bbox1(x_init - 35, y_init - 35, 70, 70);
     Rect bbox2(x_init + width_init - 35, y_init - 35, 70, 70); 
@@ -176,26 +104,22 @@ void tracking() {
     bboxes[1] = bbox2; 
     bboxes[2] = bbox3;
     bboxes[3] = bbox4; 
-    
-    // Create a tracker
+
     string trackerType = "KCF";
  
     Ptr<MultiTracker> multiTracker = MultiTracker::create();
     for (int i = 0; i < nombre_de_trackers; i++)
 		  multiTracker->add(TrackerKCF::create(), frame, Rect2d(bboxes[i]));
- 
      
     while(1)
     {     
 		Mat frame = getImage();
 		flip(frame, frame, 1);
-        //Update the tracking result with new frame
 	    bool ok = multiTracker->update(frame);
 	    
 		if (!ok)
 			putText(frame, "Error", Point(25,25), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255),2);
-	 
-	  // Draw tracked objects
+
 	  for(unsigned i=0; i<multiTracker->getObjects().size(); i++)
 	  {
 		rectangle(frame, multiTracker->getObjects()[i], 2, 1);
@@ -237,8 +161,7 @@ void tracking() {
 		}
 	  }
 	  homography = findHomography(points_finaux, points_initiaux, CV_RANSAC);
-	 
-	  // Show frame
+
 	  imshow("Camera", frame);
 	   
 	  if  (waitKey(1) == 27) break;
