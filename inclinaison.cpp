48b5e3f8bbbd988e6378cfae02e8607d1bcdb39c
DylanDIA <34124628+DylanDIA@users.noreply.github.com>
2018-10-15 12:00:45 +0200

    Update inclinaison.cpp

--- inclinaison.cpp
+++ inclinaison.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <bitset>
 #include <bits/stdc++.h>
+#include <math.h>
 
 
 using namespace cv;
@@ -22,7 +23,8 @@ using namespace std;
 
 //VARIABLES GLOBALES
 cv::Mat image ;                                   //L'image chargé dans le terminal
-cv::Mat H ;                                       //La matrice contenant l'homographie
+Mat rot_vec(3,1,cv::DataType<double>::type);      //Rotation estimée de la feuille
+Mat trans_vec(3,1,cv::DataType<double>::type);    //Translation estimée de la feuille (optionnel)
 GLsizei WIDTH = 400, HEIGHT = 400;                //Largeur et Hauteur de la fenetre
 GLfloat colors[][3] = { { 1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f }, {0.0f, 0.0f, 1.0f }, {0.0f, 0.0f, 0.0f },  {1.0f, 1.0f, 1.0f }  }; //Contient les couleurs que l'on veut utiliser en RGB
 //Index des couleurs
@@ -32,6 +34,7 @@ int blue = 2 ;
 int black = 3 ;
 int white = 4 ;
 int cpt=0 ;
+int rotation_globale_y = 0 ; //Possibilité de faire tourner l'ensemble des objets sur l'axe y pour vérifier l'inclinaison
 
 
 //Callback souris récupérant les coordonnées (x,y) d'un clic
@@ -46,30 +49,71 @@ static void onMouse(int event, int x, int y, int flags, void* param)
     }
 }
 
+void clavier(unsigned char key, int x, int y){
+
+  switch (key) {
+    case 'y': 
+      rotation_globale_y += 5 ; 
+      rotation_globale_y = rotation_globale_y % 360 ;
+      cout << rotation_globale_y << "\n" ;
+      glutPostRedisplay();
+      break ;
+  }
+
+}
+
+void def_axes(){
+  glBegin(GL_LINES) ;
+    glColor3f(colors[red][0], colors[red][1], colors[red][2]) ; //Axe X
+    glVertex3f(200, 150, 0); 
+    glVertex3f(2000, 150, 0);
+    glColor3f(colors[green][0], colors[green][1], colors[green][2]) ; //Axe Y
+    glVertex3f(200, 150, 0);
+    glVertex3f(200, 2000, 0);
+    glColor3f(colors[blue][0], colors[blue][1], colors[blue][2]) ; //Axe Z
+    glVertex3f(200, 150, 0);
+    glVertex3f(200, 150, 2000);
+  glEnd() ;
+
+}
+
 void affichage(void){
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) ; //On efface le buffer d'ecran
+  glEnable(GL_DEPTH_TEST) ; //On active le z-buffer
 
   glLoadIdentity() ;
 
   glClearColor(colors[black][0], colors[black][1], colors[black][2], 1.0f); //Couleur de fond en RGBA
 
+  glPushMatrix() ;
+    glRotatef(rotation_globale_y, 0, 1, 0) ;
+    //Rotation/Translation du repère
+    glRotated(rot_vec.at<double>(0,0), 1, 0, 0) ; //Axe X
+    glRotated(rot_vec.at<double>(1,0), 0, 1, 0) ; //Axe Y
+    glRotated(rot_vec.at<double>(2,0), 0, 0, 1) ; //Axe Z
+    //glTranslated(trans_vec.at<double>(0,0) , trans_vec.at<double>(1,0) , trans_vec.at<double>(2,0) ) ;
+    //Les axes
+    def_axes() ;
+  glPopMatrix() ;
+
   //Construction d'un rectangle blanc représentant la feuille
   glColor3f(colors[white][0], colors[white][1], colors[white][2]) ;
-  glBegin(GL_QUADS);
-    
-    glVertex2f(100.0, 100.0);
-    glVertex2f(300.0, 100.0);
-    glVertex2f(300.0, 200.0);
-    glVertex2f(100.0, 200.0);
-  glEnd() ;
-
-  //L'axe Z
-  glBegin(GL_LINES) ;
-    glColor3f(colors[red][0], colors[red][1], colors[red][2]) ;
-    glVertex3f(200, 150, -1000); //On le place au centre du rectangle
-    glVertex3f(200, 150, +1000);
-  glEnd() ;
+  glPushMatrix() ;
+    glRotatef(rotation_globale_y, 0, 1, 0) ;
+    //Rotation/Translation du repère
+    glRotated(rot_vec.at<double>(0,0), 1, 0, 0) ; //Axe X
+    glRotated(rot_vec.at<double>(1,0), 0, 1, 0) ; //Axe Y
+    glRotated(rot_vec.at<double>(2,0), 0, 0, 1) ; //Axe Z
+    //glTranslated(trans_vec.at<double>(0,0) , trans_vec.at<double>(1,0) , trans_vec.at<double>(2,0) ) ;
+    glBegin(GL_QUADS);
+        //Le rectangle
+        glVertex2f(100.0, 100.0);
+        glVertex2f(300.0, 100.0);
+        glVertex2f(300.0, 200.0);
+        glVertex2f(100.0, 200.0);
+    glEnd() ;
+  glPopMatrix() ;
 
   glFlush();
 
@@ -79,16 +123,11 @@ void affichage(void){
 
 //callback appelé à chaque fois que le fenetre est redimensionnee (une fois la fenetre crée)
 void redim(int width, int height){
-  WIDTH = glutGet(GLUT_WINDOW_WIDTH) ;
-  HEIGHT = glutGet(GLUT_WINDOW_HEIGHT) ; //Recuperation de la taille de la fenetre
   glViewport(0, 0, (GLsizei)width, (GLsizei)height) ; //"On affiche toute la fenetre"
-  //Les 3 lignes de code suivantes me permettent de remplacer les coordonees par leur equivalent en pixels...
-  //(...ce qui est fort pratique puisque l'on ne sait pas vraiment comment glVertex interprete nos coordonees...)
-  glMatrixMode(GL_PROJECTION);
+  glMatrixMode(GL_PROJECTION); //Projection sur un autre plan
   glLoadIdentity();
-  glOrtho(0.0,WIDTH,HEIGHT,0,-1.0,1.0);//On adapte les coordonees par rapport a la dimension actuelle de la fenetre
+  gluOrtho2D(0, width, 0, height) ;
   glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
 }
 
 
@@ -122,19 +161,19 @@ int main( int argc, char **argv ) {
 /*Calcul du vecteur de rotation représentant l'inclinaison de la feuille par rapport au modèle---------------------*/
 
     //Les coordonnées des coins du modèle
-    Point point_modele_1 = Point(0,0) ;
-    Point point_modele_2 = Point(991,0) ;
-    Point point_modele_3 = Point(0,715) ;
-    Point point_modele_4 = Point(991,715) ;
+    Point2f point_modele_1(0,0) ;
+    Point2f point_modele_2(991,0) ;
+    Point2f point_modele_3(0,715) ;
+    Point2f point_modele_4(991,715) ;
 
     //Les coordonnées des coins de la feuille inclinée
-    Point point_feuille_1 = Point(185,114) ;
-    Point point_feuille_2 = Point(799,149) ;
-    Point point_feuille_3 = Point(61,519) ;
-    Point point_feuille_4 = Point(977,501) ;
+    Point3f point_feuille_1(185,114,0) ;
+    Point3f point_feuille_2(799,149,0) ;
+    Point3f point_feuille_3(61,519,0) ;
+    Point3f point_feuille_4(977,501,0) ;
 
     //Création de vecteurs de points
-    std::vector<Point> obj; //Contient les points de la feuille
+    std::vector<Point3f> obj; //Contient les points de la feuille
     obj.push_back(point_feuille_1) ;
     obj.push_back(point_feuille_2) ;
     obj.push_back(point_feuille_3) ;
@@ -147,11 +186,22 @@ int main( int argc, char **argv ) {
     scene.push_back(point_modele_4) ; 
 
     //Calcul de l'homographie (=> Inclinaison)
-    H = findHomography(obj, scene, CV_RANSAC) ; //H contient l'homographie
+    cv::Mat cameraMatrix(3,3,cv::DataType<double>::type);
+    cv::setIdentity(cameraMatrix); //Valeur par défaut
 
-    //Conversion de la matrice de rotation en un vecteur de rotation
-    Mat rot_vec = Mat::zeros(1,3,CV_32F) ;
-    Rodrigues(H, rot_vec) ; //Conversion
+    cv::Mat distCoeffs(4,1,cv::DataType<double>::type);
+    distCoeffs.at<double>(0) = 0;
+    distCoeffs.at<double>(1) = 0;
+    distCoeffs.at<double>(2) = 0;
+    distCoeffs.at<double>(3) = 0; //Valeur par défaut
+
+    cv::solvePnP(obj, scene, cameraMatrix, distCoeffs, rot_vec, trans_vec);
+
+    cout << rot_vec << "\n" ;
+    cout << trans_vec << "\n" ;
+
+    //NB: rot_vec et trans_vec sont le résultat attendus par le programmme
+    //La partie visualisation ci-dessous sert de vérification
 
 
 /*Visualisation de la feuille dans un plan (OpenGL)-------------------------------------------------------------*/
@@ -173,6 +223,7 @@ int main( int argc, char **argv ) {
     //Association des callback pour cette fenetre
     glutDisplayFunc(affichage) ;
     glutReshapeFunc(redim) ;
+    glutKeyboardFunc(clavier);
 
     glutMainLoop(); //On entre dans la boucle d'evenements
 
