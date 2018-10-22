2ba1d148ff306b6a72bdb5b7c43e645a0dbb10f2
FARAUD <baptiste.faraud@gmail.com>
2018-10-22 12:52:50 +0200

    Add files via upload

--- 3dedale.cpp
+++ 3dedale.cpp
@@ -23,6 +23,9 @@ double compteur = 0; //le nombre d'image identiques à la suite
 vector <Point> points_initiaux;
 vector <Point> points_finaux;
 
+Point start;
+Point finish;
+
 void initCamera() {
 	capture = cvCaptureFromCAM( 0 );
 }
@@ -59,6 +62,65 @@ Mat image_contours(Mat frame) {
 	
 }
 
+bool findCircle(Mat image) {
+    
+    Mat gray;
+    cvtColor( image, gray, CV_BGR2GRAY);
+    vector<Vec3f> circles;
+    
+    HoughCircles( gray, circles, CV_HOUGH_GRADIENT, 1, gray.rows/8, 200, 100, 0, 0 );
+    
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        cout << "(" << circles[i][0] << "," << circles[i][1] << ") , " << circles[i][2] << endl;
+    }
+    
+    if (circles.size() != 0)
+		return true;
+    return false;
+}
+
+void findStartFinish(Mat image) {
+	Mat gray;
+	Mat blur;
+	Mat bw;
+	Mat dil;
+	Mat er;
+		
+	vector<vector<Point> > squares;
+	vector<vector<Point> > contours;
+	vector<Point> approx;
+		
+	cvtColor(image, gray, CV_BGR2GRAY);
+	medianBlur(gray, blur, 3);
+	Canny(blur, bw, 50, 150, 3);
+		
+	dilate(bw, dil, Mat(), Point(-1,-1));
+
+	findContours(dil, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+		
+	float area = 0;
+		
+	for (size_t i = 0; i < contours.size(); i++) {
+
+		approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+			
+		if(approx.size() == 3 ) {
+			finish.x = (contours[i][0].x + contours[i][1].x + contours[i][2].x) / 3;
+			finish.y = (contours[i][0].y + contours[i][1].y + contours[i][2].y) / 3;
+		}
+		
+		if(approx.size() == 4 && contours[i][0].x > 100) {
+			start.x = (contours[i][0].x + contours[i][1].x + contours[i][2].x + contours[i][3].x) / 4;
+			start.y = (contours[i][0].y + contours[i][1].y + contours[i][2].y + contours[i][3].y) / 4;
+		}
+		if(approx.size() == 5) {
+			cout << "Penta" << endl;
+		}
+	}
+	//cout << "FINISH : " << finish.x << " " << finish.y << endl;
+}
+
 void detection() {
 	Mat frame = getImage();
 	Mat image_tampon;
@@ -68,7 +130,7 @@ void detection() {
 	bool verif = false;
 	
 	double pourcentage = 0.0;
-	double incertitude = 0.96;
+	double incertitude = 0.975;
 	double valeur_max = 75;
 	
 	int largeur = frame.cols;
@@ -112,11 +174,11 @@ void detection() {
 		pourcentage = (compteur/valeur_max)*100;
 		sprintf(str,"Acquisition : %d %%",int(pourcentage));
 		
-		putText(temp, str, Point(25,25), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255),2);
+		putText(frame, str, Point(25,25), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255),2);
 		
-		rectangle(temp, pt1, pt2, (255,255,255), 5);
+		rectangle(frame, pt1, pt2, (255,255,255), 5);
 		
-		imshow( "Camera", temp );
+		imshow( "Camera", frame );
 		
 		image_tampon = temp.clone();
 		
@@ -127,57 +189,16 @@ void detection() {
 			verif = true;
 	}
 	
-	//putText(frame, "Error", Point(25,25), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255),2);
+	verif = false;
 	
-	Point p;
+	Rect myROI (pt1.x + 5, pt1.y + 5, pt2.x - 250 - 5, pt2.y - 100 - 5);
+	Mat croppedImage = frame(myROI);
+	findStartFinish(croppedImage);
 	
-	p.x = x_init;
-	p.y = y_init;
-	points_initiaux.push_back(p);
-	
-	p.x = x_init + width_init;
-	p.y = y_init;
-	points_initiaux.push_back(p);
-	
-	p.x = x_init;
-	p.y = y_init + height_init;
-	points_initiaux.push_back(p);
-	
-	p.x = x_init + width_init;
-	p.y = y_init + height_init;
-	points_initiaux.push_back(p);
-	
-}
-
-//Détection en utilisant la touche échap
-void detection_old() {
-	Mat frame = getImage();
-	int largeur = frame.cols;
-	int hauteur = frame.rows;
-
-	//Rectangle initial
-	Point pt1;
-	pt1.x = 250;
-	pt1.y = 100;
-	
-	Point pt2;
-	pt2.x = largeur - 250;
-	pt2.y = hauteur - 100;
-	
-	x_init 		= pt1.x;
-	y_init 		= pt1.y;
-	width_init  = pt2.x - pt1.x;
-	height_init = pt2.y - pt1.y;
-	
-	while(1){
-		Mat frame = getImage();
-		flip(frame, frame, 1);
-    
-		rectangle(frame, pt1, pt2, (127,255,0), 5);
-		imshow( "Camera", frame );
-		char c = cvWaitKey(33);
-		if( c == 27 ) break;
-	}
+	//finish.x += pt1.x - 5;
+	//finish.y += pt1.y - 5;
+	cout << "START  : " << start.x  << " " << start.y  << endl;
+	cout << "FINISH : " << finish.x << " " << finish.y << endl;
 	
 	Point p;
 	
@@ -231,10 +252,11 @@ void tracking() {
 	    
 		if (!ok)
 			putText(frame, "Error", Point(25,25), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255),2);
+		
 
 		for(unsigned i=0; i<multiTracker->getObjects().size(); i++)
 		{
-			rectangle(frame, multiTracker->getObjects()[i], 2, 1);
+			rectangle(frame, multiTracker->getObjects()[i], (255,255,255), 3);
 		
 			if (i == 0)
 				points_finaux.clear();
@@ -252,6 +274,9 @@ void tracking() {
 				p.x = multiTracker->getObjects()[i].x + taille_box/2;
 				p.y = multiTracker->getObjects()[i].y + taille_box/2;
 				points_finaux.push_back(p);
+				
+				putText(frame, "Finish", Point(finish.x + p.x - 50,finish.y + p.y + 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255),2);
+				putText(frame, "Start" , Point(start.x + p.x  - 50,start.y  + p.y + 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255),2);
 			}
 			
 			if (i == 1){
@@ -289,17 +314,105 @@ void tracking() {
 	
 }
 
+void afficherCercle() {
+	
+	Mat image_circle;
+	Mat img_gray;
+    
+    while(1)
+    {     
+		Mat frame = getImage();
+		flip(frame, frame, 1);
+		image_circle = frame.clone();
+		cvtColor( frame, img_gray, CV_BGR2GRAY );
+		/// Reduce the noise so we avoid false circle detection
+		GaussianBlur( img_gray, img_gray, Size(9, 9), 2, 2 );
+		vector<Vec3f> circles;
+		
+		/// Apply the Hough Transform to find the circles
+		HoughCircles( img_gray, circles, CV_HOUGH_GRADIENT, 1, img_gray.rows/8, 200, 100, 0, 0 );
+		
+		/// Draw the circles detected
+		for( size_t i = 0; i < circles.size(); i++ )
+		{
+			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			int radius = cvRound(circles[i][2]);
+			// circle center
+			circle( image_circle, center, 3, Scalar(0,255,0), -1, 8, 0 );
+			// circle outline
+			circle( image_circle, center, radius, Scalar(0,0,255), 3, 8, 0 );
+		}
+		
+		imshow( "Camera", image_circle );
+		if  (waitKey(1) == 27) break;
+	}
+}
+
+void afficherTriangle() {
+	Mat image;
+	while(1){
+		image = getImage();
+		// needed matrix
+		Mat gray;
+		Mat blur;
+		Mat bw;
+		Mat dil;
+		Mat er;
+		
+		vector<vector<Point> > squares;
+		vector<vector<Point> > contours;
+		vector<Point> approx;
+		
+		cvtColor(image, gray, CV_BGR2GRAY);
+		medianBlur(gray, blur, 3);
+		Canny(blur, bw, 50, 150, 3);
+		
+		// dilate to ensure there is no cut off lines
+		dilate(bw, dil, Mat(), Point(-1,-1));
+		
+		// find all contours
+		findContours(dil, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+		
+		float area = 0;
+		
+		// loop to find the squares
+		for (size_t i = 0; i < contours.size(); i++) {
+			
+			// approximate contour with accuracy proportional
+			// to the contour perimeter
+			approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+			//fillConvexPoly(image, approx, Scalar(255,0,255));
+			
+			if(approx.size()==4 ) { 
+				
+				if(fabs(contourArea(approx) > area)) {
+					
+					polylines( image, approx, true, Scalar(0,0,255), 2 );
+				}
+			}
+		}
+		imshow("Camera", image);
+		if  (waitKey(1) == 27) break;
+	}
+}
+
+
 int main( int argc, char* argv[] ){
 	
 	initCamera();
 	
 	cvNamedWindow( "Camera", CV_WINDOW_AUTOSIZE );
 	
+	//cvNamedWindow( "Test", CV_WINDOW_AUTOSIZE );
+	
 	/* PARTIE RECONNAISSANCE */
 	detection();
 	
 	/* PARTIE TRACKING*/
 	tracking();
+	
+	//afficherCercle();
+	//afficherTriangle();
 	
 	closeCamera();
 		
