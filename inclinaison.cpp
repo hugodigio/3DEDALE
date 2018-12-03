4e8696c0fa01af444a11f96e7fd075317f1406e6
DylanDIA <34124628+DylanDIA@users.noreply.github.com>
2018-12-03 17:27:34 +0100

    Update inclinaison.cpp

--- inclinaison.cpp
+++ inclinaison.cpp
@@ -14,60 +14,26 @@
 #include <bits/stdc++.h>
 #include <math.h>
 
+#include "inclinaison.hpp"
 
 using namespace cv;
 using namespace std;
 
 //OBJECTIF : TROUVER L'AXE Z D'UN PLAN INCLINÉ (objet) PAR RAPPORT AU MODÈLE (scene) QUI LE CONTIENT
-//ENTRÉES : - 4 points représentants les extrémités du modèle
-//          - 4 points représentants les extrémités de l'objet (plan incliné sur lequel s'affiche le labyrinthe)
+//ENTRÉES : - 4 points représentants les extrémités du modèle.
+//          - 4 points représentants les extrémités de l'objet (plan incliné sur lequel s'affiche le labyrinthe).
 //SORTIE : - 2 vecteurs (translation et rotation) relatifs à la transformation géométrique
 //           correspondant à l'inclinaison de la plan dans le modèle.
+//         - 3 valeurs (stockés dans un Vec3b euler_angles) correspondant aux angles de rotations sur les 3 axes.
+//           Ces valeurs sont calculés à partir du vecteur de rotation.
 //À partir de ces informations (notamment la rotation), on peut tracer l'axe Z image par image
-//(c.a.d. à chaque fois que les coordonnées du plan incliné changent)
+//(c.a.d. à chaque fois que les coordonnées du plan incliné changent).
 
 //compilation: make
 //test: ./inclinaison
 
-//VARIABLES GLOBALES
-cv::Mat image ;                                   //L'image du modèle
-Mat rot_vec(3,1,cv::DataType<double>::type);      //Rotation estimée de la feuille
-Mat trans_vec(3,1,cv::DataType<double>::type);    //Translation estimée de la feuille (optionnel)
-//Les entrées du programme
-//On leur donne des valeurs uniquement pour les tests
-Point2f point_modele_1(95,95) ;     // !! Point en haut à gauche !!
-Point2f point_modele_2(920,115) ;   // !! Point en haut à droite !!
-Point2f point_modele_3(80,705) ;    // !! Point en bas à gauche !!
-Point2f point_modele_4(965,690) ;   // !! Point en bas à droite !!
-
-Point2f point_objet_1(185+11,115+80) ;
-Point2f point_objet_2(795+11,150+80) ;
-Point2f point_objet_3(60+11,520+80) ;
-Point2f point_objet_4(975+11,500+80) ;
-
-//Calcul du centre du modèle (on en a besoin pour le changement de repère)
-Point2f centre((point_modele_1.x+point_modele_2.x+point_modele_3.x+point_modele_4.x/4),
-               (point_modele_1.y+point_modele_2.y+point_modele_3.y+point_modele_4.y/4)) ;
-
-//Calcul des dimensions du modèle (on en a besoin pour cameraMatrix)
-double focal_x = point_modele_2.x - point_modele_1.x ;
-double focal_y = point_modele_2.y - point_modele_1.y ;
-
-//Callback souris récupérant les coordonnées (x,y) d'un clic
-static void onMouse(int event, int x, int y, int flags, void* param)
-{
-    Mat &image = *((Mat*)param); //cast and deref the param
-
-    if (event == EVENT_LBUTTONDOWN)
-    {
-        short val = image.at< short >(y,x); // opencv is row-major ! 
-        cout << "x= " << x << " y= " << y << "val= "<<val<< endl;
-    }
-}
-
 // Checks if a matrix is a valid rotation matrix.
-bool isRotationMatrix(Mat &R)
-{
+bool isRotationMatrix(Mat &R){
     Mat Rt;
     transpose(R, Rt);
     Mat shouldBeIdentity = Rt * R;
@@ -80,8 +46,7 @@ bool isRotationMatrix(Mat &R)
 // Calculates rotation matrix to euler angles
 // The result is the same as MATLAB except the order
 // of the euler angles ( x and z are swapped ).
-Vec3f rotationMatrixToEulerAngles(Mat &R)
-{
+Vec3f rotationMatrixToEulerAngles(Mat &R){
  
     assert(isRotationMatrix(R));
      
@@ -106,61 +71,42 @@ Vec3f rotationMatrixToEulerAngles(Mat &R)
 
 }
 
+void inclinaison(vector<Point2f> modele, vector<Point2f> objet, Mat rot_mat, Mat trans_mat, Vec3f euler_angles){
 
-int main( int argc, char **argv ) {
+    assert((modele.size() == 4) && (objet.size() == 4) ) ;
 
-/*Lecture et Affichage d'une image entrée dans le terminal--------------------------------------------------*/
+    //Calcul du centre du modèle (on en a besoin pour le changement de repère)
+    Point2f centre((modele[0].x+modele[1].x+modele[2].x+modele[3].x/4),
+                (modele[0].y+modele[1].y+modele[2].y+modele[3].y/4)) ;
 
-    image = cv::imread( "feuille.jpg" ); /* Read the file */
-
-    if ( !image.data ) { /* Check for invalid input */
-        printf("Could not open or find the image\n") ;
-        return -1;
-    }
-
-    //Changement de la taille de l'image (afin qu'elle affiche ce que l'on veut voir)
-    resize(image, image, Size(), 0.25, 0.25);
-
-    cv::namedWindow( "Display window" , WINDOW_AUTOSIZE);
-    cv::imshow( "Display window", image );
-
-    //Ajout du callback onMouse à la fenêtre Display window
-    setMouseCallback("Display window", onMouse, &image); //Nous allons rentrer manuellement les coordonnées des coins de la feuille et du modèle
-
-    cv::waitKey(0); // Attente de la tappe d'une touche dans la fenêtre
-
-/*Calcul du vecteur de rotation représentant l'inclinaison de la feuille par rapport au modèle---------------------*/
+    //Calcul des dimensions du modèle (on en a besoin pour cameraMatrix)
+    double focal_x = modele[1].x - modele[0].x ;
+    double focal_y = modele[1].y - modele[0].y ;
 
     //Changement de repère (Déplacement de l'origine au centre du modèle)
-    point_modele_1 -= centre ;
-    point_modele_2 -= centre ;
-    point_modele_3 -= centre ;
-    point_modele_4 -= centre ;
+    modele[0] -= centre ;
+    modele[1] -= centre ;
+    modele[2] -= centre ;
+    modele[3] -= centre ;
 
-    point_objet_1 -= centre ;
-    point_objet_2 -= centre ;
-    point_objet_3 -= centre ;
-    point_objet_4 -= centre ;
+    objet[0] -= centre ;
+    objet[1] -= centre ;
+    objet[2] -= centre ;
+    objet[3] -= centre ;
 
     centre -= centre ;
 
     //Création de vecteurs de points
-    Point3f point_objet_1_3f(point_objet_1.x,point_objet_1.y,0) ;
-    Point3f point_objet_2_3f(point_objet_1.x,point_objet_1.y,0) ;
-    Point3f point_objet_3_3f(point_objet_1.x,point_objet_1.y,0) ;
-    Point3f point_objet_4_3f(point_objet_1.x,point_objet_1.y,0) ;
+    Point3f point_objet_1_3f(objet[0].x,objet[0].y,0) ;
+    Point3f point_objet_2_3f(objet[1].x,objet[1].y,0) ;
+    Point3f point_objet_3_3f(objet[2].x,objet[2].y,0) ;
+    Point3f point_objet_4_3f(objet[3].x,objet[3].y,0) ;
 
     std::vector<Point3f> obj; //Contient les points de l'objet (Point3f car c'est ce qu'il faut fournir à solvePnP() )
     obj.push_back(point_objet_1_3f) ;
     obj.push_back(point_objet_2_3f) ;
     obj.push_back(point_objet_3_3f) ;
-    obj.push_back(point_objet_4_3f) ;
-
-    std::vector<Point2f> scene; //Contient les points du modèle
-    scene.push_back(point_modele_1) ;
-    scene.push_back(point_modele_2) ;
-    scene.push_back(point_modele_3) ;
-    scene.push_back(point_modele_4) ; 
+    obj.push_back(point_objet_4_3f) ; 
 
     //Calcul de l'homographie (=> Inclinaison)
     cv::Mat cameraMatrix(3,3,cv::DataType<double>::type); //Conventional Camera Matrix
@@ -180,22 +126,20 @@ int main( int argc, char **argv ) {
     distCoeffs.at<double>(2) = 0;
     distCoeffs.at<double>(3) = 0; //Valeur par défaut
 
-    cv::solvePnP(obj, scene, cameraMatrix, distCoeffs, rot_vec, trans_vec);
+    Mat rot_vec(3,1,cv::DataType<double>::type);      //Stockage de la rotation estimée de la feuille
+    Mat trans_vec(3,1,cv::DataType<double>::type);    //Stockage de la translation estimée de la feuille
+
+    cv::solvePnP(obj, modele, cameraMatrix, distCoeffs, rot_vec, trans_vec);
 
     cout << rot_vec << "\n" ;
     cout << trans_vec << "\n" ;
 
-    //NB: rot_vec et trans_vec sont les résultats attendus par le programmme
-    //Grâce à ces données relatives à la transformation géometrique actuelle,
-    //on est capable de trouver l'axe Z de l'objet (=> plan incliné sur lequel figure le labyrinthe).
-
     //Conversion des vecteurs en matrices
-    Mat rot_mat , trans_mat ;
     Rodrigues(rot_vec, rot_mat) ;
     Rodrigues(trans_vec, trans_mat) ;
 
     //Calcul des angles d'Euler de la matrice de rotation
-    Vec3f euler_angles = rotationMatrixToEulerAngles(rot_mat) ;
+    euler_angles = rotationMatrixToEulerAngles(rot_mat) ;
     //Conversion radians ---> degrés
     euler_angles[0] = euler_angles[0] * (180.0/3.141592653589793238463) ;
     euler_angles[1] = euler_angles[1] * (180.0/3.141592653589793238463) ;
@@ -204,3 +148,45 @@ int main( int argc, char **argv ) {
     cout << euler_angles << "\n" ;
 
 }
+
+int main( int argc, char **argv ) {
+
+    //VARIABLES DE TEST
+    Mat rot_vec(3,1,cv::DataType<double>::type);      //Rotation estimée de la feuille
+    Mat trans_vec(3,1,cv::DataType<double>::type);    //Translation estimée de la feuille (optionnel)
+
+    //Les entrées du programme
+    Point2f point_modele_1(95,95) ;     // !! Point en haut à gauche !!
+    Point2f point_modele_2(920,115) ;   // !! Point en haut à droite !!
+    Point2f point_modele_3(80,705) ;    // !! Point en bas à gauche !!
+    Point2f point_modele_4(965,690) ;   // !! Point en bas à droite !!
+
+    vector<Point2f> modele ;
+    modele.push_back(point_modele_4) ;
+    modele.push_back(point_modele_3) ;
+    modele.push_back(point_modele_2) ;
+    modele.push_back(point_modele_1) ;
+
+    Point2f point_objet_1(185+11,115+80) ;
+    Point2f point_objet_2(795+11,150+80) ;
+    Point2f point_objet_3(60+11,520+80) ;
+    Point2f point_objet_4(975+11,500+80) ;
+
+    vector<Point2f> objet ;
+    objet.push_back(point_objet_4) ;
+    objet.push_back(point_objet_3) ;
+    objet.push_back(point_objet_2) ;
+    objet.push_back(point_objet_1) ;
+
+    //Les sorties du programmes
+    Mat rot_mat ;
+    Mat trans_mat ;
+    Vec3f euler_angles ;
+
+    //Le calcul
+    inclinaison(modele, objet, rot_mat, trans_mat, euler_angles) ;
+
+    return 0 ;
+
+}
+
