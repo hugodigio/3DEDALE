43ea62ae9eda37f2c3239c0d0075411758a74f4f
HADJI Mathias <h16023497@l-024110a018-08.salsa.univ-amu.fr>
2018-12-20 16:11:12 +0100

    header terminée

---
+++ src/Camera/background.hpp
@@ -0,0 +1,4 @@
+int chargeTextureMat(Mat image);
+void dessiner_background(float rapport, float a, float x, float y, float z, float r, float t, float l);
+void def_rectangle(float rapport);
+void fond(Mat image, Vec3f position, float far, float fov);
